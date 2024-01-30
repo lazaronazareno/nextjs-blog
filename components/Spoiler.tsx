e5fe17e02aeac7ee4170ce7d13f48231e9183627
lazaronazareno <lazaronazareno@gmail.com>
2024-01-30 17:50:59 -0300

    refactor Spoiler, modified last entry of the blog.

--- components/Spoiler.tsx
+++ components/Spoiler.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Spoiler = ({ text }) => {
   return (
-    <p className="bg-primary-400 h-full m-0 inline text-primary-400 hover:bg-transparent hover:text-inherit">
+    <span className="bg-primary-400 h-full m-0 inline text-primary-400 hover:bg-transparent hover:text-inherit">
       {text}
-    </p>
+    </span>
   )
 }
 
